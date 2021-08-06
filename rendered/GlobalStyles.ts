319198f30725f3e4b439c8f671ef4f622223ec20
rodrigoestef <rodrigoestef@gmail.com>
2021-08-06 18:49:06 -0300

    Definição de estilos padroes

--- rendered/GlobalStyles.ts
+++ rendered/GlobalStyles.ts
@@ -3,12 +3,17 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
     
     #app{
-        height: 100vh;
+        height: 100vh; 
+        background-color: #f5f5f5;
+        padding:  40px 40px;
     }
     body{   
         margin: 0;
         font-family:'Roboto';
     }
-
+    :root{
+        --primary-color:#3f51b5;
+        --hover-primary-color:#3f78b5;
+    }
 
 `;
