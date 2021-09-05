ff39f5e7ab4c3758640aa12fa534508cd1aa7ba4
Rodrigo Estef <rodrigoestef@gmail.com>
2021-09-05 00:21:56 -0300

    Correção loop

--- rendered/factorys/masks.ts
+++ rendered/factorys/masks.ts
@@ -3,7 +3,7 @@ export const cpfMask = (text: string): string => {
   newText = newText.replace(/\D/g, "");
   newText = newText.replace(/(^\d{11})(\d)/, "$1");
   newText = newText.replace(/(\d)(\d{2}$)/, "$1-$2");
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
     newText = newText.replace(/(\d)(\d{3})(\D)/, "$1.$2$3");
   }
   return newText;
