4ff667408aaf9a519b61ddfe3cfa1de1cc59d80d
rodrigoestef <rodrigoestef@gmail.com>
2022-03-02 17:19:51 -0300

    Adc sincronização do banco de dados

--- main/conection.ts
+++ main/conection.ts
@@ -1,12 +1,19 @@
 import { createConnection } from "typeorm";
 import { resolve } from "path";
+import { app } from "electron";
 
 export default async () => {
+  const pathUser =
+    String(process.env.NODE_ENV).trim() === "development"
+      ? "."
+      : app.getPath("userData");
+  console.log(pathUser);
   await createConnection({
     type: "sqlite",
-    database: "./database.sqlite",
-    synchronize: false,
+    database: `${pathUser}/database.sqlite`,
+    synchronize: true,
     logging: false,
+    migrations: [resolve(__dirname, "./migration/*")],
     entities: [resolve(__dirname, "./models/*")],
   });
 };
